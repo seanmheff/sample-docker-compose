a95b9de8737feb319efe5662b690da9c6f9f3dcf
Sean Heffernan <seanmheff@gmail.com>
2016-08-02 22:30:03 -0400

    Start on getting app to work

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var app = require('express')();
-var http = require('http').Server(app);
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : 'bspec',
+  password : process.env.DB_PASSWORD,
+  database : 'bspec'
 });
 
-http.listen(3000, function(){
+var http = require('http').Server(app);
+
+connection.connect(function(err) {
+  if (!err) {
+    console.log("Database is connected...");  
+  } else {
+    console.log("Error connecting database... ", err);  
+  }
+});
+
+app.get('/', function(req, res) {
+  connection.query('SELECT * from products LIMIT 2', function(err, rows, fields) {
+    connection.end();
+    if (!err) { res.json(rows); }
+    else { res.json(err); }
+  });
+});
+
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
